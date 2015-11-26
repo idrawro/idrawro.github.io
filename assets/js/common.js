be604002a00601b64f88c733ce8347ca5a18a53b
emilianionascu <emilian.ionascu@gmail.com>
2015-11-27 00:02:17 +0200

    Hid the logo on scroll

---
+++ assets/js/common.js
@@ -0,0 +1,12 @@
+﻿jQuery(document).ready(function () {
+    var scrollBar = jQuery('paper-scroll-header-panel')[0];
+    var logo = jQuery('.logoIronImage');
+
+    scrollBar.addEventListener('content-scroll', function () {
+        if (scrollBar.headerState === Polymer.PaperScrollHeaderPanel.HEADER_STATE_EXPANDED) {
+            logo.show();
+        } else {
+            logo.hide();
+        }
+    });
+})
