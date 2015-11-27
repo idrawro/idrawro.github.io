caa464b9ed061d8a9ca253dc33ee5ed9202ff286
emilianionascu <emilian.ionascu@gmail.com>
2015-11-27 10:25:40 +0200

    Updated Mandatory Logo

--- assets/js/common.js
+++ assets/js/common.js
@@ -9,4 +9,13 @@
             logo.hide();
         }
     });
+
+    var buttonSendContactMessage = jQuery('#buttonSendContactMessage');
+    var form = jQuery('#contactForm');
+
+    buttonSendContactMessage.click(function () {
+        console.log(jQuery("#messageBody").val());
+        //jQuery.post("http://contactapi-idraw.rhcloud.com/contactApi", form.serialize());
+        jQuery.post("http://localhost:8080/contactApi", form.serialize());
+    });
 })
